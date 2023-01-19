4bc44460bcc20f7a44a2d8522fd37d9a9dec39fe
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-01-19 23:17:07 +0000

    added Logical Operators to WhatShouldIWear.java

--- src/main/java/WhatShouldIWear.java
+++ src/main/java/WhatShouldIWear.java
@@ -5,27 +5,33 @@ public class WhatShouldIWear {
 
         // Prompt User on: Weather and Temperature.
 
-        System.out.println("What words would describe the weather today?");
-        System.out.println("sunny, cloudy, rainy, snowy");
+        System.out.println("What word would describe the weather today?");
+        System.out.println("please select from: sunny, cloudy, rainy and snowy.");
 
-        Scanner reader1 = new Scanner(System.in);
-        String userWeather = reader1.nextLine();
-
-
-
+        Scanner wReader = new Scanner(System.in);
+        String userWeather = wReader.nextLine();
 
         System.out.println("what is the average temperature for the day?");
-        System.out.println("please enter a value between -10 and 30 degrees.");
+        System.out.println("please enter a number to represent degrees.");
 
-        Scanner reader2 = new Scanner(System.in);
-        int userTemperature = Integer.parseInt(reader2.nextLine());
+        Scanner tReader = new Scanner(System.in);
+        int userTemperature = Integer.parseInt(tReader.nextLine());
 
+        System.out.println("The weather is looking " + userWeather + " and the temperature seems to be around " + userTemperature + "\u00B0");
 
+        if (userWeather.equals("sunny") && userTemperature >= 20){
+            System.out.println("Its hot outside, consider short sleeves, sunglasses or a cap");
+            else if (userWeather.equals("cloudy") && userTemperature >= 10)
+                System.out.println("its warm outside, don't overdress and keep it cool");
+                else if (userWeather.equals("rainy") || userTemperature <= 9){
+                    System.out.println("Don't forget your umbrella");
+                    else (userWeather.equals("snowy") || userTemperature <= 5){
+                    System.out.println("Its really cold! Please take a beanie, scarf and gloves");
+                }
+                }
+                }
+            }
+        }
 
-        // allow users to input data.
-
-        // return suggestion.
-
-        System.out.println("The weather is looking " + userWeather + "and the temperature seems to be around " + userTemperature + "\u00B0");
     }
 }
