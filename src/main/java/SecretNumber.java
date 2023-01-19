7d8dddbebbe4786c5c5c8b0addf315967b4699ef
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-01-19 19:25:13 +0000

    added controlFlow to SecretNumber.java

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -7,29 +7,46 @@ public class SecretNumber {
         // Prompt user to input a random number between 1 and 100.
 
         System.out.println("Feeling lucky!? Guess my secret number");
-        System.out.println("Hint its between 1 and 100");
+        System.out.println("Hint: its between 1 and 100");
 
-        // generate random number
+        // generate random number.
 
         Random rand = new Random();
         int upperbound = 100;
         int secretInt = rand.nextInt(upperbound);
         System.out.println(secretInt);
 
-
         // use scanner to accept input.
 
         Scanner reader = new Scanner(System.in);
-        String userGuess = reader.nextLine();
+        int userGuess = Integer.parseInt(reader.nextLine());
+        System.out.println("Your guess was: " + userGuess);
+
+        //let user know if the userGuess was too high or too low.
+        //set a winState boolean
+
+
+        String winState = "Congratulations! You won!".toUpperCase();
+        String loseState = "Please try again";
+
+
+        if (userGuess == secretInt){
+            System.out.println(winState);
+        } else if (userGuess < secretInt) {
+            System.out.println("A little too low, guess higher!");
+        } else if (userGuess > secretInt) {
+            System.out.println("A little too high, guess lower!");
+        }
+
+
+
 
         // reject if string is input. error message "Thats not a number!"
         // reject if int is higher than 100. error message "Lower than 100 please."
-        // let user know if the number was too high or too low or correct
         //if too high or too low, allow the user to input again.
         // win message.
 
-//        System.out.println("Feeling lucky!? Guess my secret number");
-//        System.out.println("Hint its between 1 and 100");
+
 
 
 
